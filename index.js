c1ed855faa5f352381fa0d0039ef7e36e9c86889
Agustin Simon <agustinsimon@Mac.fibertel.com.ar>
2025-01-16 16:58:54 -0300

    fixed env variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Importa cors
+import cors from 'cors';
 import prospectsRouter from './routes/prospects.js';
 
 dotenv.config();
